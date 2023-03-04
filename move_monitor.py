11d90a1ed9c20d9e9dc13ff6a534a5f08f041656
Crozzers <captaincrozzers@gmail.com>
2023-03-04 18:22:48 +0000

    Add ability to set which monitor you are moving relative to
    Also add ability to move the primary monitor through a little bit of hackery

--- move_monitor.py
+++ move_monitor.py
@@ -14,29 +14,51 @@ ctypes.windll.user32.SetProcessDPIAware()
 Sides = Literal['top', 'left', 'bottom', 'right']
 
 
-def calc_fuzzy_pos(devices: list[Monitor], index: int, pos: Sides, align: Sides = 'center'):
-    primary = [i for i in devices if i.is_primary][0]
+def get_monitors():
+    primary = None
+    monitors = []
+    for monitor in screeninfo.get_monitors():
+        if monitor.is_primary:
+            primary = monitor
+        else:
+            monitors.append(monitor)
+    monitors.sort(key=lambda a: a.name)
+    monitors.insert(0, primary)
+    return monitors
+
+
+def flip_args(pos: Sides) -> Sides:
+    flip = {
+        'center': 'center',
+        'top': 'bottom',
+        'left': 'right'
+    }
+    flip.update({v: k for k, v in flip.items()})
+
+    return flip[pos]
+
+
+def calc_fuzzy_pos(base: Monitor, device: Monitor, pos: Sides, align: Sides = 'center'):
     x, y = 0, 0
-    device: Monitor = devices[index]
     new_mode = pywintypes.DEVMODEType()
     if pos in ('top', 'bottom'):
         if align == 'left':
-            x = primary.x
+            x = base.x
         elif align == 'right':
-            x = primary.width - device.width
+            x = base.width - device.width
         else:
-            x = (primary.width // 2) - (device.width // 2)
+            x = (base.width // 2) - (device.width // 2)
 
-        y = primary.height if pos == 'bottom' else primary.y - device.height
+        y = base.height if pos == 'bottom' else base.y - device.height
     else:
         if align == 'top':
-            y = primary.y
+            y = base.y
         elif align == 'bottom':
-            y = primary.height - device.height
+            y = base.height - device.height
         else:
-            y = (primary.height // 2) - (device.height // 2)
+            y = (base.height // 2) - (device.height // 2)
 
-        x = primary.width if pos == 'right' else primary.x - device.width
+        x = base.width + base.x if pos == 'right' else base.x - device.width
 
     new_mode.Position_x = x
     new_mode.Position_y = y
@@ -55,20 +77,29 @@ if __name__ == '__main__':
         Sides), help='Move to this side of the primary display')
     move_parser.add_argument('align', nargs='?', default='center', choices=get_args(
         Sides) + ('center',), help='[optional] align to this edge of the chosen side')
+    move_parser.add_argument('--rel-to', default=0, type=int,
+                             help='move the monitor relative to the position of this one')
     args = parser.parse_args()
 
     # make sure primary monitor is monitor 0, then sort the rest by "\\DISPLAY1..." str
-    devices = sorted(
-        sorted(screeninfo.get_monitors(), key=lambda a: a.name),
-        key=lambda a: a.is_primary, reverse=True
-    )
+    devices = get_monitors()
 
     if args.command == 'list':
         for index, device in enumerate(devices):
             print(index, ':', device)
     else:
-        choice: Monitor = devices[args.display]
-        mode = calc_fuzzy_pos(devices, args.display, args.side, args.align)
+        if args.display == 0:
+            # dont move the primary, it causes issues. Move the other one relative to
+            # the primary instead
+            choice = devices[args.rel_to]
+            rel_to = devices[0]
+            pos, align = flip_args(args.side), args.align
+        else:
+            choice = devices[args.display]
+            rel_to = devices[args.rel_to]
+            pos, align = args.side, args.align
+
+        mode = calc_fuzzy_pos(rel_to, choice, pos, align)
 
         print(win32api.ChangeDisplaySettingsEx(
             choice.name, mode, 0))
